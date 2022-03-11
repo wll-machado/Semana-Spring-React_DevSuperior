abbc75f7b220d3080ca8b976acb6660e349b39f9
Well <w9llington@gmail.com>
2022-03-11 15:41:20 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/edevsuperior/dvmovie/DvmovieApplication.java
@@ -0,0 +1,13 @@
+package com.edevsuperior.dvmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvmovieApplication.class, args);
+	}
+
+}
