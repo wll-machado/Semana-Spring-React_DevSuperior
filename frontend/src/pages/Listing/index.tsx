f8883241e17c283332c9abd3b659647ef6c1a838
Well <w9llington@gmail.com>
2022-03-12 16:23:09 -0300

    MovieScore

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,12 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing(){
     return(
         <>
             <Pagination/>
+
+            <MovieStars/>
         </>
     )
 }
